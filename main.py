cf466c08874ca3a0bc0cde0037176145c318251d
Berat Bagir <beratbagir03@gmail.com>
2024-04-24 20:22:39 +0300

    main.py added

---
+++ main.py
@@ -0,0 +1,68 @@
+import tkinter
+from tkinter import *
+
+window = Tk()
+window.title("BMI Calculator")
+window.minsize(width=300, height=250)
+
+def calculate_bmi():
+    height = height_input.get()
+    weight = weight_input.get()
+
+    if weight == "" or height == "":
+        result_label.config(text = "Enter both weight and height!")
+    else:
+        try:
+            bmi = float(weight) / (float(height) / 100) **2
+            result_string = write_result(bmi)
+            result_label.config(text=result_string)
+        except:
+            result_label.config(text="Enter a valid number")
+
+
+#label
+my_label = Label(text="Enter Your Weight (kg)")
+my_label.config(fg="black")
+my_label.pack()
+weight_input = tkinter.Entry(width=10)
+weight_input.pack()
+my_label2 = Label(text="Enter Your Height (cm)")
+my_label2.config(fg="black")
+my_label2.pack()
+height_input = tkinter.Entry(width=10)
+height_input.pack()
+
+
+def write_result(bmi):
+    result_string = f"Your BMI is: {bmi}. You are"
+    if bmi <=16:
+        result_string += "severely thin!"
+    elif bmi > 16 and bmi <= 17:
+        result_string += "moderately thin!"
+    elif bmi > 17 and bmi <= 18.5:
+        result_string += "mild thin"
+    elif bmi > 18.5 and bmi <= 25:
+        result_string += "normal"
+    elif bmi > 25 and bmi <= 30:
+        result_string += "overweight"
+    elif bmi > 30 and bmi <= 35:
+        result_string += "obese class 1"
+    elif bmi > 35 and bmi <= 40:
+        result_string += "obese class 2"
+    else:
+        result_string += "obese class 3"
+    return result_string
+
+
+
+#button
+my_button = tkinter.Button(text="Calculate", command=calculate_bmi)
+my_button.pack()
+
+result_label = tkinter.Label()
+result_label.pack()
+
+
+
+
+window.mainloop()
